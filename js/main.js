c0e3709e02791565b01a5b0cc35076dcd1fedd0e
ARIF1925 <taufiqarif19@gmail.com>
2026-01-19 23:46:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ function start(){
   if(!name) return alert("Nama jangan kosong 😝");
 
   localStorage.setItem("username", name);
-  document.getElementById("music").play();
 
-  setTimeout(()=>location.href="puzzle.html",700);
+  const music = document.getElementById("music");
+  music.play().then(() => {
+    setTimeout(() => {
+      location.href = "puzzle.html";
+    }, 700);
+  }).catch(err => {
+    console.log("Autoplay blocked:", err);
+    location.href = "puzzle.html";
+  });
 }
