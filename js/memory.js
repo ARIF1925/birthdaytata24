abc7171e079d7f601b847b80aefa847fcd1af312
ARIF1925 <taufiqarif19@gmail.com>
2026-01-20 00:13:26 +0700

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,8 +1,5 @@
 function check() {
-  const val = document.getElementById("answer").value.toLowerCase();
-  if (val === "ayang") {
-    window.location.href = "map.html";
-  } else {
-    alert("Salah 😝");
-  }
+  const v = answer.value.toLowerCase();
+  if (v === "ayang") location.href = "map.html";
+  else alert("Coba inget lagi 😝");
 }
