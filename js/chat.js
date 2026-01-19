99db0d5c8581b9d234bbb92b8a9407ddd61e17e9
ARIF1925 <taufiqarif19@gmail.com>
2026-01-20 00:05:06 +0700

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,33 @@
-function finish(){
-  location.href="final.html";
+const chat = document.getElementById("chat");
+let step = 0;
+
+function add(text, type="bot") {
+  const d = document.createElement("div");
+  d.className = "msg " + type;
+  d.innerText = text;
+  chat.appendChild(d);
+  chat.scrollTop = chat.scrollHeight;
+}
+
+add("Hai ayang 💛");
+add("Tanggal jadian kita?");
+
+function send() {
+  const v = msg.value.toLowerCase();
+  add(msg.value,"user");
+  msg.value="";
+
+  if (step===0 && v==="0506") {
+    step++;
+    add("Inget dong 😭");
+    add("Warna favorit kamu?");
+    return;
+  }
+
+  if (step===1 && ["kuning","biru","merah"].includes(v)) {
+    window.location.href="final.html";
+    return;
+  }
+
+  add("Hmm salah 😆");
 }
